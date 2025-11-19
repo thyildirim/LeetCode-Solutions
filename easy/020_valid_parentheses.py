441f5c5b0824989696d89a7c26090c39ee92303b
thyildirim <taha0yldrm@outlook.com>
2025-11-19 21:20:15 +0300

    solution code

---
+++ easy/020_valid_parentheses.py
@@ -0,0 +1,34 @@
+"""
+LeetCode Problem 20: Valid Parentheses
+Difficulty: Easy
+Link: https://leetcode.com/problems/valid-parentheses
+"""
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for char in s:
+            if char in "({[":
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                top = stack.pop()
+                if char == ')' and top != '(':
+                    return False
+                if char == '}' and top != '{':
+                    return False
+                if char == ']' and top != '[':
+                    return False
+
+        return not stack
+
+a = Solution()
+print(a.isValid("()"))
+print(a.isValid("()[]{}"))
+print(a.isValid("(]"))
+print(a.isValid("([)]"))
+print(a.isValid("{[]}"))
+print(a.isValid("{{{((()))}}]"))
+print(a.isValid("{{{((()))}}}"))
+
