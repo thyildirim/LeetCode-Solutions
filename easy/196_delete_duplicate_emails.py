3e8c7df8c4a11ec0775ee120add679320fe41ea5
thyildirim <taha0yldrm@outlook.com>
2025-11-22 23:34:20 +0300

    solution code

--- easy/196_delete_duplicate_emails.py
+++ easy/196_delete_duplicate_emails.py
@@ -4,4 +4,12 @@ Difficulty: Easy
 Link: https://leetcode.com/problems/delete-duplicate-emails
 """
 
+delete from person where id not in (
+    select MIN(id)
+    from person
+    group by email
+)
+
+# Keep only the email with the smallest id.
+# Sql command
 
